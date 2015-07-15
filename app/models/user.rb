77eeb91681d543bc9aedce6b66457cd048b5c35f
judas iscariot <reckmobile@gmail.com>
2015-07-15 21:44:50 +0100

    initial tools and configurations - devise, heroku etc

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # ::trackable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :confirmable, :validatable
+end

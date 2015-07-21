08ff6d92cc1879411119829835b1b2609869b034
judas iscariot <reckmobile@gmail.com>
2015-07-21 16:48:19 +0100

    little fiddling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ActionMailer::Base
   
-
   def sample_email(user)
-     @user = user
+    @user = user
     mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
     message_params = {:from    => @user.email,
                       :to      => 'sorting@app8572295d9f7d4a2ca16a86b732ce74b5.mailgun.org',

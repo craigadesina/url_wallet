77fb4a146b2726c6e9bd51f0629f5b1b10374402
judas iscariot <reckmobile@gmail.com>
2015-07-21 16:03:16 +0100

    resetting mailer again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ActionMailer::Base
   def sample_email(user)
      @user = user
     mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
-    message_params = {:from    => "wally wally",
-                      :to      => 'reckmobile@gmail.com',
+    message_params = {:from    => @user.email,
+                      :to      => "#{User.sample}@app8572295d9f7d4a2ca16a86b732ce74b5.mailgun.org",
                       :subject => 'First Topic. awesome. Testing Mailgun recieving API',
                       :text    => 'https://github.com/mailgun/mailgun-ruby'}
     mg_client.send_message ENV['DOMAIN'], message_params
--- config/initializers/development_mail_interceptor.rb
+++ config/initializers/development_mail_interceptor.rb
@@ -1,7 +1,7 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
-    #message.to =  "admino@app8572295d9f7d4a2ca16a86b732ce74b5.mailgun.org"
-    #message.cc = nil
-    #message.bcc = nil
+    message.to =  "admino@app8572295d9f7d4a2ca16a86b732ce74b5.mailgun.org"
+    message.cc = nil
+    message.bcc = nil
   end
 end

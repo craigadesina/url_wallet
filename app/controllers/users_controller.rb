51a37328ea5bb2d92409b5f8cbf70c2f2522f624
judas iscariot <reckmobile@gmail.com>
2015-07-28 12:54:29 +0100

    adding upvoted_books to users show#view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @topics = @user.topics
+    @upvoted_bookmarks = @user.bookmarks
   end
 
   def email
